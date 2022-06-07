3fd8f5bf14789b9f2fa5b5706d497d4decee84b7
shikdershondhi <ehatesham.ul-islam@welldev.io>
2022-06-07 17:53:35 +0600

    search box added

--- cypress/e2e/Welltravel/TravelApp/Flight/sample_spec.js
+++ cypress/e2e/Welltravel/TravelApp/Flight/sample_spec.js
@@ -2,34 +2,19 @@
 describe('login', () => {
   it('should redirect to the correct page after logging in', () => {
     cy.login('user', { cacheSession: false })
-    // function validate() {
-    //   cy.request('/api/user').its('status').should('eq', 200)
-    // }
+    cy.validate()
   })
 })
 
-      describe('flight search page',() =>
+      describe('flight Book page',() =>
       {
-          it('flight info', () =>
+        it('Search box', () =>
           {
-
-            //cy.get('.flight-form__drop-downs > :nth-child(1) > .menu > .menu__item > .icon-button').click()
-            //cy.get(':nth-child(1) > .menu > .menu__item > .menu__list > :nth-child(1) > [data-testid="wrapperWithoutLink"]').click()
-            cy.get('.flight-form__right-field > .select-box > .select-box-wrapper > .select-box-container > .select-box__control > .select-box__indicators > .select-box__indicator > .css-19bqh2r').click()
-            cy.get('#react-select-4-input').type('cpt')
-            cy.wait(1000)
-            cy.get('.flight-form__left-field > .select-box > .select-box-wrapper > .select-box-container > .select-box__control > .select-box__indicators > .select-box__indicator > .css-19bqh2r').click()
-            cy.get('#react-select-5-input').type('lhr')
-            cy.wait(1000)
+            cy.select_trip()
+            cy.select_travelers()
+            cy.origin_start()
+            cy.destination()
+            cy.start_date()
+            cy.search()
           })
-
-        })
-
-        describe('flight search', () =>
-        {
-          it('flight select',()=>
-          {
-
-
-          })
-        })
+      })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+import { times } from 'lodash';
 
 Cypress.Commands.add('login', (name, { cacheSession = true } = {}) => {
 	const login = () => {
@@ -13,3 +14,44 @@ Cypress.Commands.add('login', (name, { cacheSession = true } = {}) => {
 		login()
 	}
 })
+
+Cypress.Commands.add('start_date', () => {
+	cy.get('.flight-form__right-field > .date-picker > .SingleDatePicker > :nth-child(1) > .SingleDatePickerInput > .SingleDatePickerInput_calendarIcon > .icon > svg').click().wait(2000)
+	cy.get('.flight-form__right-field > .date-picker > .SingleDatePicker > :nth-child(1) > .SingleDatePickerInput > .SingleDatePickerInput_clearDate > .SingleDatePickerInput_clearDate_svg > path').click().wait(2000)
+	cy.get('body > main:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > form:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)').type('14 July 2022').wait(2000)
+})
+
+Cypress.Commands.add('select_trip', () => {
+	cy.get('.flight-form__drop-downs > :nth-child(1) > .menu > .menu__item > .icon-button > .icon-button__icon > .icon > svg').click().wait(2000) // trip selection
+	cy.get(':nth-child(1) > .menu > .menu__item > .menu__list > :nth-child(1) > [data-testid="wrapperWithoutLink"]').click().wait(1000)  // one way selection
+})
+
+Cypress.Commands.add('select_travelers', () => {
+	// select travelers
+	times(0, () => {
+		cy.get(':nth-child(1) > .quantity-selector > :nth-child(3) > .quantity-selector__icon > .icon > svg > path').should('exist').click().wait(1000)
+	})
+	cy.get('.search-container__form').click().wait(2000)
+})
+Cypress.Commands.add('origin_start', () => {
+	cy.get('.flight-form__right-field > .select-box > .select-box-wrapper > .select-box-container > .select-box__control > .select-box__indicators > .select-box__indicator > .css-19bqh2r').click().wait(1000)
+	cy.get('#react-select-4-input').type('cpt').wait(2000)
+})
+Cypress.Commands.add('destination', () => {
+	cy.get('.flight-form__left-field > .select-box > .select-box-wrapper > .select-box-container > .select-box__control > .select-box__indicators > .select-box__indicator > .css-19bqh2r').click().wait(1000)
+	cy.get('#react-select-5-input').type('lhr').wait(1000)
+	cy.get('.search-container').click().wait(2000)
+})
+Cypress.Commands.add('search', () => {
+	cy.get('.flight-form__bottom-right-action-bar > .button--primary').click().wait(20000)
+})
+Cypress.Commands.add('visit_travelapp', () => {
+	cy.visit('https://wellautotng.staging.welltravel.com/en-GB/')
+
+})
+Cypress.Commands.add('validate', () => {
+	function validate() {
+		cy.request('/api/user').its('status').should('eq', 200)
+	}
+})
+
