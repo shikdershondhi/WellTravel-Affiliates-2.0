d740eb2380ce257bff664d412e8d93363e41baae
shikdershondhi <ehatesham.ul-islam@welldev.io>
2022-06-12 11:03:16 +0600

    tag2.0: added ccv

---
+++ cypress/e2e/Welltravel/TravelApp/Flight/FlightBooking_Single_Route&Traveler_ccv.js
@@ -0,0 +1,39 @@
+describe('visit', () => {
+	it('should redirect to the correct page', () => {
+		cy.visit_travelapp()
+	})
+	it('should login', () => {
+		cy.get('.col-y-bleed > .justify-space-between > :nth-child(3)').then(($body) => {
+			if ($body.text().includes('Log in')) {
+				cy.travelapp_login()
+			} else {
+				cy.travelapp_Book_now()
+			}
+		})
+	})
+})
+describe('flight Book flow',() =>
+{
+	it('Search box', () => {
+		cy.travelapp_select_trip()
+		cy.travelapp_select_travelers()
+		cy.travelapp_origin_start()
+		cy.travelapp_destination()
+		cy.travelapp_start_date()
+		cy.travelapp_search()
+	})
+	it('flight result page', () => {
+		cy.travelapp_single_flight_type()
+		cy.travelapp_select_farebrand_unknown()
+		cy.travelapp_continue_to_cart()
+	})
+	it('cart', () => {
+		cy.travelapp_Your_Cart()
+		cy.travelapp_Travelers_Information_mybooking()
+		cy.travelapp_add_card()
+		cy.travelapp_Add_New_Card_part_one()
+		cy.travelapp_switchToIframe("#\\#welldev-booking-iframe")
+		cy.travelapp_Add_New_Card_part_two()
+		cy.travelapp_Review_book_continue_to_order()
+	})
+})
