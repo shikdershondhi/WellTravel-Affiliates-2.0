7668e1adf31938a31c22f70cc7e8cc123ec20355
M. Ehatesham ul islam <ehatesham.ul-islam@welldev.io>
2022-09-01 18:23:40 +0600

    pay_later

--- cypress/support/approval_process/approval_process_commands.js
+++ cypress/support/approval_process/approval_process_commands.js
@@ -86,3 +86,42 @@ Cypress.Commands.add('approval_process_post_and_pdf_download',()=>{
 		}
 	})
 })
+
+//==================pay_later=================
+
+Cypress.Commands.add('approval_process_pre_process_pay_later',()=>{
+	cy.get('.stepper').then(($body) => {
+		if ($body.text().includes('Request Approval')) {
+			cy.get('.stepper__footer > .button > span').click().wait(1000)
+			cy.travelapp_logout()
+			cy.travelapp_login_approver_one()
+			cy.get('.top-nav__avatar > .rcl-avatar > .rcl-avatar__content').click()
+			cy.get(':nth-child(1) > [data-testid="wrapperWithLink"]').click().wait(3000)
+			cy.get(':nth-child(3) > .link > .link__content').click()
+			cy.get('.pill-tabs > :nth-child(1) > :nth-child(1) > .rtabs__nav-container > :nth-child(2)').click().wait(1000)
+			cy.get('.pill-tabs > :nth-child(1) > :nth-child(1) > .rtabs__nav-container > :nth-child(1)').click().wait(1000)
+			cy.get('.request-list__rtabs > .rtabs > .rtabs__nav > .rtabs__nav-container > :nth-child(2)').click().wait(2000)
+			cy.get('.request-list__rtabs > .rtabs > .rtabs__nav > .rtabs__nav-container > :nth-child(1)').click().wait(2000)
+			cy.get(':nth-child(1) > .table__field--align-center > .table__field-value > .link > .link__content > .icon-button').click().wait(1000)
+			cy.scrollTo('top',{ensureScrollable: false}).wait(2000)
+			cy.get('.request__heading-approve-button > .button').click().wait(1000)
+			cy.travelapp_logout()
+			cy.travelapp_login().wait(1000)
+			cy.get('.top-nav__avatar > .rcl-avatar > .rcl-avatar__content').click().wait(1000)
+			cy.get(':nth-child(1) > [data-testid="wrapperWithLink"]').click().wait(2000)
+			cy.get('.col-12 > :nth-child(3) > .link').click().wait(2000)
+			cy.get('.pill-tabs > :nth-child(1) > :nth-child(1) > .rtabs__nav-container > :nth-child(2)').click().wait(2000)
+			cy.get('.request-list__rtabs > .rtabs > .rtabs__nav > .rtabs__nav-container > :nth-child(2)').click().wait(2000)
+			cy.get(':nth-child(1) > .table__field--align-center > .table__field-value > .link > .link__content > .icon-button').click().wait(1000)
+			cy.scrollTo('top',{ensureScrollable: false}).wait(2000)
+			cy.get('.request__heading-status > :nth-child(2) > .button').click().wait(1000)
+			cy.get('.deal-customize__warning--text > .button').click().wait(50000)
+			cy.travelapp_Your_Cart()
+			cy.travelapp_Travelers_Information_mybooking()
+			cy.travelapp_Add_Payment_Later()
+
+		} else {
+			cy.travelapp_Add_Payment_Later()
+		}
+	})
+})
--- cypress/support/travelapp/travelapp_ccv.js
+++ cypress/support/travelapp/travelapp_ccv.js
@@ -2,7 +2,12 @@ import 'cypress-iframe'
 
 //==============invoice================
 Cypress.Commands.add('travelapp_invoice', () => {
-	cy.get('.paymentsidebar__option-right > .radio > .radio__check').click().wait(3000)
+	cy.get(':nth-child(2) > .paymentsidebar__option-right > .radio > .radio__check').click().wait(3000)
+	cy.get('.button--tertiary > :nth-child(2)').click().wait(3000)
+})
+
+Cypress.Commands.add('travelapp_Add_Payment_Later', () => {
+	cy.get(':nth-child(1) > .paymentsidebar__option-right > .radio > .radio__check').click().wait(3000)
 	cy.get('.button--tertiary > :nth-child(2)').click().wait(3000)
 })
 //============== CCV================
